4fcd2cfaf425e567c505d4bd439e2df6ea945e61
Piper Thunstrom <pathunstrom@gmail.com>
2025-06-13 15:44:43 -0700

    fix linting.

--- src/dykes/__init__.py
+++ src/dykes/__init__.py
@@ -1,6 +1,4 @@
-import dykes.options
-import dykes.internal
-import dykes.utils
-import dykes.processing
 from dykes.processing import parse_args, build_parser
 from dykes.options import Action, Count, StoreFalse, StoreTrue
+
+__all__ = ["options", "parse_args", "build_parser", "Action", "Count", "StoreFalse", "StoreTrue"]
